b9518ba6858ebf1f1c56c10fd9df6229e7adc3cd
Radhika Kumari <143119058+RadhikaCongreja@users.noreply.github.com>
2025-01-03 16:00:03 +0500

    Handle integration

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useReducer, useState } from "react";
+import "./App.css";
+import AwardList from "./AwardList";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const app = useReducer();
+
+  <AwardList></AwardList>;
+
+  return <></>;
+}
+
+export default App;
